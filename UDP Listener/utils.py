57db84b53648d7bbb489439cdd76c95af4d9ff29
LeSamourai23 <ayaan.ahmad@learner.manipal.edu>
2025-06-11 03:07:00 +0530

    Fixing git issue

--- UDP Listener/utils.py
+++
@@ -1,9 +0,0 @@
-def seconds_to_lap_time(seconds):
-    prefix = ""
-    if seconds < 0:
-        prefix = "-"
-        seconds *= -1
-
-    minutes = seconds // 60
-    remaining = seconds % 60
-    return prefix + "{:01.0f}:{:06.3f}".format(minutes, remaining)
